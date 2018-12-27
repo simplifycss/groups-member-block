457352e82631dab0123a92215a02d58ecced46e4
DSlavcheva <46031230+DSlavcheva@users.noreply.github.com>
2018-12-27 21:24:07 +0200

    Update groups-member-block.php

--- groups-member-block.php
+++ groups-member-block.php
@@ -1,5 +1,24 @@
 <?php
 /**
+ *
+ * @link              https://author.url
+ * @since             1.0.0
+ * @package           Groups_Member_Block
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Groups Member Block
+ * Plugin URI:        https://plugin.site
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Denitsa Slavcheva
+ * Author URI:        https://author.url
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       groups-member-block
+ * Domain Path:       /languages
+ */
+
+/**
  * Functions to register client-side assets (scripts and stylesheets) for the
  * Gutenberg block.
  *
